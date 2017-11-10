eabddea107c6f9fed247884e443f98a255b6af2a
Ross Scarborough <rossscarborough@gmail.com>
2017-11-10 15:08:53 -0500

    Set up scss files and added up/down vote function.

--- app.js
+++ app.js
@@ -11,7 +11,16 @@ app.controller('MainCtrl', [
       {title: 'post 5', upvotes: 4}
     ];
     $scope.addPost = function(){
+      if(!$scope.title || $scope.title === '') {
+        return;
+      };
       $scope.posts.push({title: $scope.title, upvotes: 0});
       $scope.title = '';
+    };
+    $scope.incrementUpvotes = function(post) {
+      post.upvotes += 1;
+    };
+    $scope.incrementDownvotes = function(post) {
+      post.upvotes -= 1;
     };
   }]);
