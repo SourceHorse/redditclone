24cbcf5fd95804e7dab20ffe176f69396295c05d
Ross Scarborough <rossscarborough@gmail.com>
2017-11-11 18:18:08 -0500

    Routes for preloading and upvoding

--- reddit-clone/routes/index.js
+++ reddit-clone/routes/index.js
@@ -19,7 +19,7 @@ router.get('/posts', function(req, res, next) {
   });
 });
 
-router.post('/posts', function(req, res, next) {
+router.post('/posts/', function(req, res, next) {
   var post = new Post(req.body);
   post.save(function(err, post) {
     if(err) {
@@ -27,6 +27,24 @@ router.post('/posts', function(req, res, next) {
     }
     res.json(post);
   });
+});
+
+router.param('post', function(req, res, next, id) {
+  var query = Post.findById(id);
+  query.exec(function (err, post) {
+    if (err) {
+      return next(err);
+    }
+    if (!post) {
+      return next(new Error('can\'t find post'));
+    }
+    req.post = post;
+    return next();
+  });
+});
+
+router.get('/posts/:post', function(req, res) {
+  res.json(req.post);
 });
 
 module.exports = router;
