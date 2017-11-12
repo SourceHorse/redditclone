ed985d6e2919bf2abb0c59083d313dfaa022366c
Ross Scarborough <rossscarborough@gmail.com>
2017-11-12 18:25:05 -0500

    Wired up authentication to posting, commenting, and up/downvoting.

--- reddit-clone/routes/index.js
+++ reddit-clone/routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
+var jwt = require('express-jwt');
 var router = express.Router();
+var auth = jwt({secret: 'SECRET', userProperty: 'payload'});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -10,6 +12,7 @@ var mongoose = require('mongoose');
 var passport = require('passport');
 var Post = mongoose.model('Post');
 var Comment = mongoose.model('Comment');
+var User = mongoose.model('User');
 
 router.post('/register', function(req, res, next){
   if(!req.body.username || !req.body.password){
@@ -29,6 +32,22 @@ router.post('/register', function(req, res, next){
   });
 });
 
+router.post('/login', function(req, res, next){
+  if(!req.body.username || !req.body.password){
+    return res.status(400).json({message: 'Please fill out all fields'});
+  }
+
+  passport.authenticate('local', function(err, user, info){
+    if(err){ return next(err); }
+
+    if(user){
+      return res.json({token: user.generateJWT()});
+    } else {
+      return res.status(401).json(info);
+    }
+  })(req, res, next);
+});
+
 router.get('/posts', function(req, res, next) {
   Post.find(function(err, posts){
     if(err) {
@@ -38,8 +57,10 @@ router.get('/posts', function(req, res, next) {
   });
 });
 
-router.post('/posts/', function(req, res, next) {
+router.post('/posts/', auth, function(req, res, next) {
   var post = new Post(req.body);
+  post.author = req.payload.username;
+
   post.save(function(err, post) {
     if(err) {
       return next(err);
@@ -85,7 +106,7 @@ router.get('/posts/:post', function(req, res, next) {
   });
 });
 
-router.put('/posts/:post/upvote', function(req, res, next) {
+router.put('/posts/:post/upvote', auth, function(req, res, next) {
   req.post.upvote(function(err, post){
     if (err) {
       return next(err);
@@ -94,7 +115,7 @@ router.put('/posts/:post/upvote', function(req, res, next) {
   });
 });
 
-router.put('/posts/:post/downvote', function(req, res, next) {
+router.put('/posts/:post/downvote', auth, function(req, res, next) {
   req.post.downvote(function(err, post){
     if (err) {
       return next(err);
@@ -103,7 +124,7 @@ router.put('/posts/:post/downvote', function(req, res, next) {
   });
 });
 
-router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
+router.put('/posts/:post/comments/:comment/upvote', auth, function(req, res, next) {
   req.comment.upvote(function(err, comment){
     if (err) {
       return next(err);
@@ -112,7 +133,7 @@ router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
   });
 });
 
-router.put('/posts/:post/comments/:comment/downvote', function(req, res, next) {
+router.put('/posts/:post/comments/:comment/downvote', auth, function(req, res, next) {
   req.comment.downvote(function(err, comment){
     if (err) {
       return next(err);
@@ -121,9 +142,10 @@ router.put('/posts/:post/comments/:comment/downvote', function(req, res, next) {
   });
 });
 
-router.post('/posts/:post/comments', function(req, res, next) {
+router.post('/posts/:post/comments', auth, function(req, res, next) {
   var comment = new Comment(req.body);
   comment.post = req.post;
+  comment.author = req.payload.username;
 
   comment.save(function(err, comment) {
     if(err) {
