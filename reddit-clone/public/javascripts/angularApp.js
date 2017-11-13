4e496e487d23e9f0e546782d4e0ecd3132db9ca4
Ross Scarborough <rossscarborough@gmail.com>
2017-11-12 20:19:06 -0500

    Added navgation buttons.

--- reddit-clone/public/javascripts/angularApp.js
+++ reddit-clone/public/javascripts/angularApp.js
@@ -128,6 +128,15 @@ app.config([
     };
   }])
 
+  .controller('NavCtrl', [
+  '$scope',
+  'auth',
+  function($scope, auth){
+    $scope.isLoggedIn = auth.isLoggedIn;
+    $scope.currentUser = auth.currentUser;
+    $scope.logOut = auth.logOut;
+  }]);
+
 
   app.factory('posts', ['$http', function($http) {
     var o = {
